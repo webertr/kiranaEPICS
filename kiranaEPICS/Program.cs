579ec0b632994560e79cbc03f7600bbcfd155d46
webertr <webert2@uw.edu>
2019-05-03 13:07:50 -0700

    Just playing around with ways to control the Kirana software.

--- kiranaEPICS/Program.cs
+++ kiranaEPICS/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
 
 namespace kiranaEPICS
 {
+
     class Program
     {
+        // Get a handle to an application window.
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
         static void Main(string[] args)
         {
+            // Get a handle to the Calculator application. The window class
+            // and window name were obtained using the Spy++ tool.
+            //IntPtr kiranaHandle = FindWindow("TfrmKTestMain", "Kirana");
+            IntPtr kiranaHandle = FindWindow("TAboutDlg", "About");
+
+            // Verify that Calculator is a running process.
+            if (kiranaHandle == IntPtr.Zero)
+            {
+                System.Console.WriteLine("Did not find window");
+            } else
+            {
+                System.Console.WriteLine("Found window");
+            }
+
+            SetForegroundWindow(kiranaHandle);
+
+            System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+
+            System.Console.ReadKey();
+
+            // Make Calculator the foreground application and send it 
+            // a set of calculations.
+            //SetForegroundWindow(calculatorHandle);
+            //System.Windows.Forms.SendKeys.SendWait("111");
+
         }
     }
 }
